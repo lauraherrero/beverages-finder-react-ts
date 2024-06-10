635ed0e1d5558f6bc046618eae171c8c0d157910
Laura Herrero Poza <lherrero@nextdigital.es>
2024-06-10 18:45:34 +0200

    use hook uselocation to know the info about the route and add form in Header component and condition to show or hide it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { useMemo } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 
 export const Header = () => {
+
+  const { pathname } = useLocation();
+  
+  const isHome = useMemo(() => pathname === '/', [pathname]);
+  console.log(isHome);
+  
+  
+    
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
@@ -10,10 +19,39 @@ export const Header = () => {
             <img className="w-32" src="/logo.svg" alt="logotipo cocktail" />
           </div>
           <nav className='flex gap-4'>
-            <Link className='text-white uppercase font-bold' to="/">Inicio</Link>
-            <Link className='text-white uppercase font-bold' to="/favoritos">Favoritos</Link>
+            <NavLink className={({isActive}) => isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold' } to="/">Inicio</NavLink>
+            <NavLink className={({isActive}) => isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold' } to="/favoritos">Favoritos</NavLink>
           </nav>
         </div>
+        {isHome && (
+          <form
+            className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'
+          >
+            <div className='space-y-4'>
+              <label htmlFor="ingredient" className='block, text-white uppercase font-extrabold text-lg'>Nombre o Ingredientes</label>
+              <input 
+                type="text" 
+                name="ingredient" 
+                id="ingredient" 
+                className='p-3 w-full rounded-lg focus:outline-none'
+                placeholder='Nombre o Ingrediente. Ej: Vodka, Tequila, Café...'
+              />
+            </div>
+            <div className='space-y-4'>
+              <label htmlFor="category" className='block, text-white uppercase font-extrabold text-lg'>Categoría</label>
+              <select 
+                name="category" 
+                id="category" 
+                className='p-3 w-full rounded-lg focus:outline-none'
+              >
+                <option value="">--Seleccione--</option>
+              </select>
+            </div>
+            <input type="submit" value="Buscar recetas"
+              className='cursor-pointer bg-orange-700 hover:bg-orange-800 text-white font-extrabold w-full p-2 rounded-lg uppercase'
+            />
+          </form>
+        )}
       </div>
     </header>
   )
